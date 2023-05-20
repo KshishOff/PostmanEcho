816e3b6f0876a09c75e6b82ea1a203d8aec0ab65
KshishOff <krasavinmaxx@gmail.com>
2023-05-20 14:51:32 +0300

    False

--- src/test/java/ru/netology/PostmanEcho/PostmanEchoPostTest.java
+++ src/test/java/ru/netology/PostmanEcho/PostmanEchoPostTest.java
@@ -17,6 +17,6 @@ public class PostmanEchoPostTest {
                 .post("/post")
                 .then()
                 .statusCode(200)
-                .body("data", equalTo("incredibly"));
+                .body("data", equalTo("ncredibly"));
     }
 }
